0164ab907b88f3279711ec7b74ff77b92ca609bf
JuneCho23 <junecho.ucb@gmail.com>
2017-03-15 15:26:42 -0700

    Automation Test Scripts for Montage

---
+++ Montage/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace Montage
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			// TODO: If the Android app being tested is included in the solution then open
+			// the Unit Tests window, right click Test Apps, select Add App Project
+			// and select the app projects that should be tested.
+			app = ConfigureApp
+				.Android
+				// TODO: Update this path to point to your Android app and uncomment the
+				// code if the app is not included in the solution.
+				.ApkFile("/Users/junecho/Desktop/Montage.apk")
+				.StartApp();
+		}
+
+		[Test]
+		public void AppLaunches()
+		{
+
+			app.Tap(x => x.Class("AIRWindowSurfaceView"));
+			app.DismissKeyboard();
+			app.Screenshot("AIRWindowSurfaceView");
+
+		}
+	}
+}
+
